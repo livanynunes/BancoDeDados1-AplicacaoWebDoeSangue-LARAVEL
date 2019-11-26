ae5de63bc08768b84aa4fb5922b7420b4a2dcd5a
Livany <40469622+livanynunes@users.noreply.github.com>
2019-11-26 14:36:07 -0300

    Multiple authetication - Users and doador tables - part 2
    - Correção direcionamento após autenticação do usuário.

--- doesangue_web/app/Http/Middleware/RedirectIfAuthenticated.php
+++ doesangue_web/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,12 +17,27 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+        // if (Auth::guard($guard)->check()) {
+        //     return redirect('/home');
+        // }
+        // if ($guard == "doador" && Auth::guard($guard)->check()) {
+        //         return redirect('/doador');
+        //     }
+
+        switch ($guard) {
+            case 'doador':
+                if (Auth::guard($guard)->check()) {
+                    // return redirect()->route('doador.dashboard');
+                    return redirect('/doador');
+                }
+                break;
+            
+            default:
+                 if (Auth::guard($guard)->check()) {
+                    return redirect('/home');
+                }
+                break;
         }
-        if ($guard == "doador" && Auth::guard($guard)->check()) {
-                return redirect('/doador');
-            }
 
         return $next($request);
     }
